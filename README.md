fe74f4aec712066dee581f7d2ae824e45d82c34c
MatejKastak <matej.kastak@gmail.com>
2021-06-08 22:08:37 +0200

    yara: Add base support for the language

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # pygments-yara-lexer
 YARA language lexer for pygments.
+
+## How to run?
+
+Currently the package is not deployed to public index. You must install it
+locally using the following steps:
+
+```bash
+git clone https://github.com/MatejKastak/pygments-yara-lexer
+poetry install
+poetry run pygmentize file.yar
+```
---
+++ pygments_yara_lexer/yara.py
@@ -0,0 +1,22 @@
+from pygments.lexer import RegexLexer
+from pygments.token import *
+
+
+class YaraLexer(RegexLexer):
+    name = "YARA"
+    aliases = ["yara", "yar"]
+    filenames = ["*.yar", "*.yara"]
+
+    tokens = {
+        "root": [
+            (r"//.*$", Comment.Single),
+            (r"import", Keyword),
+            (r"rule", Keyword),
+            (r"(meta:|strings:|condition:)", Keyword),
+            (r"\$\w+", Name.Variable),
+            (r'"', String.Double, "string_text"),
+            (r"/", String.Regex, "string_regexp"),
+        ],
+        "string_text": [(r'[^"]', String.Double), (r'"', String.Double, "#pop")],
+        "string_regexp": [(r"[^/]", String.Regex), (r"/", String.Regex, "#pop")],
+    }
